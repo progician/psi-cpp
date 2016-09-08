4f7bd2d34e645a0beadc99ac5d670ec2dd92acc6
progician <progician@progician.me>
2016-09-08 16:29:18 +0100

    Remove all unnecessary code
    The original idea behind the project is pretty sound however this isn't
    the right place to do this. In preparation to move the code to a public
    share to pair with the lisp version, trim the code so that it only does
    what it needs to show the basic concept.
    
    Remove all unnecessary bits and includes.

--- peer.cpp
+++ peer.cpp
@@ -1,43 +1,15 @@
-#include "elgamal.hpp"
-#include "finite-group.hpp"
-#include "polynomial.hpp"
 #include "private-intersection.hpp"
 
-#include <algorithm>
-
-#include <boost/asio.hpp>
-#include <boost/program_options.hpp>
-
-#include <fstream>
 #include <iostream>
 #include <set>
-#include <stdexcept>
+#include <tuple>
 #include <vector>
-
-namespace program_options = boost::program_options;
-using boost::asio::ip::tcp;
-
-
-std::set< int32_t >
-buildInputSet( std::istream* inputStream )
-{
-	std::set< int32_t > results;
-	while ( !inputStream->eof() ) {
-		int32_t inputNumber = -1;
-		(*inputStream) >> inputNumber;
-		if ( inputStream->fail() )
-			continue;
-		results.insert( inputNumber );
-	}
-
-	return results;
-}
 
 
 std::ostream&
 operator <<( std::ostream& ostr, std::set< int64_t > const& v )
 {
-	for ( const int32_t n: v ) {
+  for ( const int64_t n: v ) {
 		ostr << n << " ";
 	}
 
@@ -46,75 +18,19 @@ operator <<( std::ostream& ostr, std::set< int64_t > const& v )
 
 
 int
-main( int argc, char** argv )
+main( int, char** )
 {
-  /*program_options::options_description description;
-	description.add_options()
-    ( "port,p", program_options::value<uint16_t>()->default_value( 6666 ),
-			"The shared memory identifier through which the PSI peers communicate" )
-		( "help,h", "produce this help message" )
-		( "input-set-file", "input set file name. optional, if missing it will default to the standard input" );
-
-	program_options::positional_options_description positionalOptions;
-	positionalOptions.add( "input-set-file", 1 );
-
-	program_options::variables_map variablesMap;
-	program_options::store(
-	  program_options::command_line_parser( argc, argv )
-		  .options( description )
-		  .positional( positionalOptions )
-		  .run(), variablesMap );
-	program_options::notify( variablesMap );
-
-	if ( variablesMap.count( "help" ) > 0 ) {
-		std::cout << description << std::endl;
-		return 0;
-	}
-
-	std::istream* inputStream = &std::cin;
-	bool cleanUpInputStream = false;
-	if ( variablesMap.count( "input-set-file" ) > 0 ) {
-		inputStream = new std::ifstream( variablesMap[ "input-set-file" ].as<std::string>() );
-		if ( inputStream->fail() ) {			
-			std::cerr << "error: " << strerror( errno ) << std::endl;
-			delete inputStream;
-			return 0;
-		}
-		cleanUpInputStream = true;
-	}
-
-	std::set< int32_t > inputSet = buildInputSet( inputStream );
-	if ( cleanUpInputStream )
-		delete inputStream;
-
-  uint16_t portNumber = variablesMap[ "port" ].as<uint16_t>();
-
-  std::cout << "port number: " << boost::lexical_cast<std::string>( portNumber ) << std::endl; */
-  // std::cout << "input set: " << inputSet << std::endl;
-
   std::vector< int64_t > const clientSet = { 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22 };
   std::vector< int64_t > const serverSet = { 3, 6, 9, 12, 15, 18, 21 };
   fg::Group cryptoGroup( 2250635938ull, 3 );
 
   auto const keys = ElGamal::keygen( cryptoGroup );
+
   auto const fromClient = PrivateIntersection::prepareLocalSet( clientSet.begin(), clientSet.end(), std::get<1>( keys ) );
-
-  for ( auto const e : clientSet ) {
-    auto const fe = cryptoGroup( e );
-    ElGamal::Cipher const res = poly::eval( fe, fromClient.begin(), fromClient.end() );
-    fg::Elem const decrypted = ElGamal::decrypt( std::get<0>( keys ), res );
-    if ( decrypted != cryptoGroup.plusOne() )
-      std::cerr << "failed root: " << e << std::endl;
-  }
-
-
   auto const fromServer = PrivateIntersection::obliviousEvaluation( serverSet.begin(), serverSet.end(), fromClient );
   auto const intersection = PrivateIntersection::extractIntersection< int64_t, std::vector< int64_t >::const_iterator >( clientSet.begin(), clientSet.end(), fromServer, std::get<0>( keys ) );
 
   std::cout << intersection << std::endl;
-
-  // std::cout << std::endl;
-  // std::cout << "Looking for counter part for PSI calculation..." << std::endl;
 
 	return 0;
 }
