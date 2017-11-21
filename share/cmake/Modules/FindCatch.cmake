60199ee3f3fb6e249f67bde3f94b37f53f64e2c1
Gyula Gubacsi <gyula.gubacsi@progician.me>
2017-11-21 08:00:17 +0000

    Add Catch library dependency for unit testing

---
+++ share/cmake/Modules/FindCatch.cmake
@@ -0,0 +1,14 @@
+find_path( Catch_ROOT
+  NAMES
+    include/catch/catch.hpp
+  DOC
+    "The relative root of the Catch C++ unit testing framework." )
+
+  if ( ${Catch_ROOT} STREQUAL "Catch-NOTFOUND")
+  message( FATAL_ERROR "Catch C++ unit testing framework was not found" )
+endif()
+
+add_library( Catch::catch INTERFACE IMPORTED )
+set_property( TARGET Catch::catch
+  PROPERTY
+    INTERFACE_DIRECTORIES ${Catch_ROOT}/include )
