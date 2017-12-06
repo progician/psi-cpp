da1a008c8e2f92f1ff70ea675ab07a3dd414724e
Gyula Gubacsi <gyula.gubacsi@progician.me>
2017-12-06 22:24:41 +0000

    Adding cyclic ring elements

--- unittest/CyclicGroupTest.cpp
+++ unittest/CyclicGroupTest.cpp
@@ -2,13 +2,43 @@
 
 
 struct SimpleCyclicGroupTraits {
-  static const int64_t order = 0x800000;
-  static const int64_t generator = 2;
+  static constexpr int64_t Order = 0x800000;
+  static constexpr int64_t Generator = 2;
+  static constexpr int64_t AdditiveIdentity = 0;
 };
 
 
+template< typename Traits >
+  class Elem {
+    int64_t const ordinalIndex_;
+
+  public:
+    constexpr Elem( const int64_t ordinalIndex )
+      : ordinalIndex_( ordinalIndex < Traits::Order
+          ? ordinalIndex
+          : throw std::runtime_error( "ordinalIndex exceeds ring's order" ) ) { } 
+
+    constexpr Elem()
+      : ordinalIndex_( Traits::AdditiveIdentity ) {}
+
+    bool operator ==( Elem const& other ) const {
+      return ordinalIndex_ == other.ordinalIndex_;
+    }
+
+
+    Elem< Traits > operator+( Elem< Traits > const& other ) {
+      return Elem< Traits > {
+        ( ordinalIndex_ + other.ordinalIndex_ ) % Traits::Order
+      };
+    }
+  };
+
+
+
 TEST_CASE( "In cyclic groups" ) {
-  SECTION( "when elements are added" ) {
-    
+  SECTION( "uninitialized element is identity for adding" ) {
+    Elem< SimpleCyclicGroupTraits > a { 3 };
+    Elem< SimpleCyclicGroupTraits > b;
+    REQUIRE( ( a + b ) == a );
   }
 }
